2e7a934bfa7afbe282c79029041ede6a66052aad
Хабиб <minecraft11242006@gmail.com>
2022-10-21 18:17:47 +0500

    add login page

--- auth/views.py
+++ auth/views.py
@@ -4,4 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def register(request):
     if request.method == "GET":
-        return render(request, 'auth/base.html')
+        return render(request, 'auth/register.html')
+
+
+def login(request):
+    if request.method == "GET":
+        return render(request, 'auth/login.html')
